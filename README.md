7dd351e2addd86ce620a544d7c136b2da7ceed4e
Libster <al@memeglobal.com>
2017-08-14 13:08:29 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Vue Simple Toggle (Switch)
+
+Simple multi-select component with order - you can not only manage a set of items (like a regular multi-select) but also their order.
+
+No sass/less compilation. Very easy to use :).
+
+<img src="https://raw.githubusercontent.com/AlexLibs/vue-libs-simple-toggle/master/demo/vue-libs-simple-toggle-demo.png" /><br>
+
+## Installation
+Add the dependencies to your html file:
+```
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/3.4.8/select2.min.js"></script>
+```
+
+```bash
+npm install vue-libs-multi-select-with-order --save
+```
+
+## Basic Usage
+
+```javascript
+import MultiSelectWithOrder from 'vue-libs-multi-select-with-order';
+
+new Vue({
+
+    components: {
+        MultiSelectWithOrder
+    },
+
+    data () {
+        return {
+            keys: ['white', 'red', 'blue'],
+            availableKeys: ['orange', 'yellow', 'green', 'white', 'red', 'blue', 'black', 'brown']
+        }
+    }
+};
+```
+
+```html
+            <multi-select-with-order class="ordered-keys" v-model="keys"
+                                     v-bind:items="availableKeys">
+            </multi-select-with-order>
+```
+
+## Customization
+
+Override the relevant css classes to customize it.
---
+++ vue_libs_multi_select_with_order.vue
@@ -0,0 +1,162 @@
+<template>
+    <div class="multi-select-with-order" v-bind:class="{'all-items-in-use': allItemsInUse, 'hidden-list': hiddenList}">
+
+        <div class="multi-select-items" v-if="showMultiSelectItems">
+            <div v-for="item in selectValue" class="multi-select-item" v-bind:data-item="item">
+                <span class="item-text">{{ item }}</span>
+                <div class="close">&times;</div>
+            </div>
+        </div>
+
+        <span class="add-button">+</span>
+        <select>
+            <option></option>
+            <option v-bind:value="item" v-for="item in itemsNotInUse">{{ item }}</option>
+        </select>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                selectValue: this.value,
+                hiddenList: this.hiddenList,
+                showMultiSelectItems: true
+            };
+        },
+        mounted: function () {
+            this.hiddenList = true;
+
+            const self = this;
+            const selectEl = $(this.$el).children('select');
+
+            function initItemsContainer() {
+                const multiSelectItems = $(self.$el).children('.multi-select-items');
+
+                multiSelectItems.disableSelection();
+
+                multiSelectItems.delegate('.close', 'click', function (event, myName, myValue) {
+                    self.selectValue.splice($(this).parent().index(), 1);
+                    self.$emit('input', self.selectValue);
+                });
+
+                const sortableContainer = $(self.$el).children('.multi-select-items');
+
+                sortableContainer.sortable({
+                    update: function(event, ui) {
+                        self.selectValue = ui.item.parent()
+                            .children('.multi-select-item')
+                            .map(function() {return $(this).attr('data-item')})
+                            .get();
+                        self.showMultiSelectItems = false;
+                        setTimeout(function() {
+                            self.showMultiSelectItems = true;
+                            setTimeout(function() {
+                                self.$emit('input', self.selectValue);
+                                initItemsContainer();
+                            }, 0);
+                        }, 0);
+                    }
+                });
+            }
+            initItemsContainer();
+
+            selectEl
+                .select2()
+                .on('change', function (evt) {
+                    self.selectValue.push(selectEl.select2('val'));
+                    self.$emit('input', self.selectValue);
+                    selectEl.select2('val', '');
+                    self.hiddenList = true;
+                });
+            $(this.$el).children('.add-button')
+                .click(function () {
+                    self.hiddenList = false;
+                    setTimeout(function() {
+                        selectEl.select2('open');
+                        $('.select2-input').on( "blur", function() {
+                            self.hiddenList = true;
+                        });
+                    }, 0);
+                });
+        },
+        props: [
+            'value',
+            'items'
+        ],
+        watch: {
+            value: function (val) {
+                this.selectValue = val;
+            }
+        },
+        methods: {},
+        components: {},
+        computed: {
+            itemsNotInUse: function () {
+                return this.items.filter(item => this.selectValue.indexOf(item) == -1);
+            },
+            allItemsInUse: function () {
+                return this.itemsNotInUse.length === 0;
+            }
+        }
+    }
+</script>
+
+<style>
+    @import url('//cdnjs.cloudflare.com/ajax/libs/select2/3.4.8/select2.css');
+    @import url('//cdnjs.cloudflare.com/ajax/libs/select2/3.4.8/select2-bootstrap.css');
+    .multi-select-with-order {
+        border: 1px solid #dbdbdb;
+        border-radius: 4px;
+        padding: 10px;
+    }
+    .multi-select-with-order .add-button {
+        font-size: 28px;
+        font-weight: bold;
+        vertical-align: middle;
+        cursor: pointer;
+        display: none;
+        vertical-align: super;
+    }
+    .multi-select-with-order .multi-select-items {
+        display: inline-block;
+    }
+    .multi-select-with-order .multi-select-items .multi-select-item {
+        vertical-align: bottom;
+        display: inline-block;
+        border: 1px solid #dbdbdb;
+        border-radius: 4px;
+        padding: 4px;
+        margin: 4px;
+        background-color: white;
+        cursor: pointer;
+    }
+    .multi-select-with-order .multi-select-items .multi-select-item .close {
+        display: inline-block;
+        border-left: 1px solid #dbdbdb;
+        cursor: pointer;
+        padding: 0 1px 0 4px;
+        font-weight: bold;
+        font-size: 20px;
+    }
+    .multi-select-with-order .multi-select-items .multi-select-item .item-text {
+        padding-right: 4px;
+    }
+    .multi-select-with-order .select2-container {
+        margin: 8px;
+        width: 200px;
+    }
+    .multi-select-with-order.hidden-list .select2-container {
+        display: none;
+    }
+    .multi-select-with-order.hidden-list .add-button {
+        display: inline;
+    }
+    .multi-select-with-order.all-items-in-use .select2-container {
+        display: none;
+    }
+    .multi-select-with-order.all-items-in-use .add-button {
+        display: none;
+    }
+</style>
